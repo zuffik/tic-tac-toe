f56390182c4a2cd4ad27ba4d22bfa6c9d8a8010f
Kristián Žuffa <kristian.zuffa@gmail.com>
2020-03-21 13:03:59 +0100

    update ci

--- game/Assets/Editor/BuildScript.cs
+++ game/Assets/Editor/BuildScript.cs
@@ -11,21 +11,17 @@ public class BuildScript
         var buildDirectory = Environment.CurrentDirectory + Path.PathSeparator + "Build" + Path.PathSeparator;
         var winBuildDirectory = buildDirectory + "Win" + Path.PathSeparator;
         var webGLBuildDirectory = buildDirectory + "WebGL" + Path.PathSeparator;
-        Directory.CreateDirectory(buildDirectory);
-        Directory.CreateDirectory(winBuildDirectory);
-        Directory.CreateDirectory(webGLBuildDirectory);
         Debug.Log(buildDirectory);
         Debug.Log(winBuildDirectory);
         Debug.Log(winBuildDirectory);
         Debug.Log("*** Building Windows ***");
         var report = BuildPipeline.BuildPlayer(
-            new[] {"Scenes/MainScene.unity"},
+            new[] {"Assets/Scenes/MainScene.unity"},
             winBuildDirectory + "game.exe",
             BuildTarget.StandaloneWindows64,
             BuildOptions.None
         );
-        Debug.Log("Windows report:");
-        Debug.Log(report);
+        Debug.Log("Windows files:");
         foreach (var f in report.files)
         {
             Debug.Log(f.path);
@@ -34,18 +30,16 @@ public class BuildScript
 
         Debug.Log("*** Building WebGL ***");
         report = BuildPipeline.BuildPlayer(
-            new[] {"Scenes/MainScene.unity"},
+            new[] {"Assets/Scenes/MainScene.unity"},
             webGLBuildDirectory,
             BuildTarget.WebGL,
             BuildOptions.None
         );
-        Debug.Log("WebGL report:");
-        Debug.Log(report);
+        Debug.Log("WebGL files:");
         foreach (var f in report.files)
         {
             Debug.Log(f.path);
         }
-
-
+        EditorApplication.Exit(Directory.Exists(buildDirectory) ? 0 : 1);
     }
 }
