733b365e40b1fe628016e44b90523308e9c0609a
Kristián Žuffa <kristian.zuffa@gmail.com>
2020-03-21 12:08:04 +0100

    update ci

--- game/Assets/Scripts/BuildScript.cs
+++ game/Assets/Scripts/BuildScript.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -6,26 +7,37 @@ public class BuildScript
 {
     static void PerformBuild()
     {
-        Debug.Log(Environment.CurrentDirectory);
+        var buildDirectory = Environment.CurrentDirectory + Path.PathSeparator + "Build" + Path.PathSeparator;
+        var winBuildDirectory = buildDirectory + "Win" + Path.PathSeparator;
+        var webGLBuildDirectory = buildDirectory + "WebGL" + Path.PathSeparator;
+        Directory.CreateDirectory(buildDirectory);
+        Directory.CreateDirectory(winBuildDirectory);
+        Directory.CreateDirectory(webGLBuildDirectory);
+        Debug.Log(buildDirectory);
+        Debug.Log(winBuildDirectory);
+        Debug.Log(winBuildDirectory);
         Debug.Log("*** Building Windows ***");
         var report = BuildPipeline.BuildPlayer(
             new[] {"Assets/MainScene.unity"},
-            Environment.CurrentDirectory + "/Build/Win/game.exe",
+            winBuildDirectory + "game.exe",
             BuildTarget.StandaloneWindows64,
             BuildOptions.None
         );
         Debug.Log("Windows report:");
         Debug.Log(report);
+        Debug.Log(report.files);
+        
 
         Debug.Log("*** Building WebGL ***");
         report = BuildPipeline.BuildPlayer(
             new[] {"Assets/MainScene.unity"},
-            Environment.CurrentDirectory + "/Build/WebGL/",
+            webGLBuildDirectory,
             BuildTarget.WebGL,
             BuildOptions.None
         );
         Debug.Log("WebGL report:");
         Debug.Log(report);
+        Debug.Log(report.files);
 
 
     }
